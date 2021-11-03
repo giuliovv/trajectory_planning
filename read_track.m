2f41ab4a2c6c82ca3f8e323f290c410c3b673e2c
Giulio Vaccari <giulio09@marcov.it>
2021-11-03 18:17:54 +0100

    All new racetracks

--- read_track.m
+++ read_track.m
@@ -3,11 +3,10 @@ close all
 clc
 track = readtable("tracks/Monza.csv");
 n = height(track);
-%leftpoints = zeros(n, 2);
-%rightpoints =  zeros(n, 2);
-for point = 1:n-1
-    %A(point, :) = get_border(track.x_X_m(point), track.y_m(point), track.x_X_m(point+1), track.y_m(point+1), track.w_tr_left_m(point), track.w_tr_left_m(point+1), track.w_tr_right_m(point), track.w_tr_right_m(point+1));
-    [leftpoints(point, :), rightpoints(point, :)] = get_border(track.x_X_m(point), track.y_m(point), track.x_X_m(point+1), track.y_m(point+1), track.w_tr_left_m(point), track.w_tr_left_m(point+1), track.w_tr_right_m(point), track.w_tr_right_m(point+1));
+leftpoints = zeros(n, 2);
+rightpoints =  zeros(n, 2);
+for idx = 1:n-1
+    [leftpoints(idx, :), rightpoints(idx, :)] = get_border(track.x_X_m(idx), track.y_m(idx), track.x_X_m(idx+1), track.y_m(idx+1), track.w_tr_left_m(idx), track.w_tr_left_m(idx+1), track.w_tr_right_m(idx), track.w_tr_right_m(idx+1));
 end
 hold on
 plot(leftpoints(:, 1), leftpoints(:, 2))
