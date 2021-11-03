89e8f09b587808aec8d99e4a857c7e9bc09737bc
Giulio Vaccari <giulio09@marcov.it>
2021-11-03 19:19:46 +0100

    Test with points distance

--- read_track.m
+++ read_track.m
@@ -5,9 +5,14 @@ track = readtable("tracks/Monza.csv");
 n = height(track);
 leftpoints = zeros(n, 2);
 rightpoints =  zeros(n, 2);
+oldleft = [0,0];
+oldright = [0,0];
 for idx = 1:n-1
-    [leftpoints(idx, :), rightpoints(idx, :)] = get_border(track.x_X_m(idx), track.y_m(idx), track.x_X_m(idx+1), track.y_m(idx+1), track.w_tr_left_m(idx), track.w_tr_left_m(idx+1), track.w_tr_right_m(idx), track.w_tr_right_m(idx+1));
+    [leftpoints(idx, :), rightpoints(idx, :)] = get_border(track.x_X_m(idx), track.y_m(idx), track.x_X_m(idx+1), track.y_m(idx+1), track.w_tr_left_m(idx), track.w_tr_left_m(idx+1), track.w_tr_right_m(idx), track.w_tr_right_m(idx+1), oldleft, oldright);
+    oldleft(:) = leftpoints(idx);
+    oldright(:) = rightpoints(idx);
 end
+
 hold on
 plot(leftpoints(:, 1), leftpoints(:, 2))
 plot(rightpoints(:, 1), rightpoints(:,2))
