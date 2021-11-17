ba1ab5e5b458e045ded1f6564a9a87ee339af53b
Giulio Vaccari <giulio09@marcov.it>
2021-11-17 18:45:39 +0100

    Connected track generator to main

--- main.m
+++ main.m
@@ -6,8 +6,8 @@ clc
 w=1.992;
 e=0.40;
 
-%qui dobbiamo introdurre il circuito (abbiamo bisogno dei parametri n e
-%destra e sinistra), basta mettere il comando che fa partire il circuito?
+%track borders
+[rightpoints, leftpoints] = read_track("tracks/Monza.csv");
 
 %initialization of the coefficient alpha
 alpha = 0.5*zeros(n);
