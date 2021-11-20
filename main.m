69f79135c60cd824865af0b782340e487c9c2f77
Giulio Vaccari <giulio09@marcov.it>
2021-11-20 16:41:04 +0100

    Clean up and random initialization of alpha

--- main.m
+++ main.m
@@ -11,7 +11,7 @@ e=0.40;
 n = height(rightpoints);
 
 %% Initialization of the coefficient alpha
-alpha0 = 0.5*ones(n);
+alpha0 = rand(n, 1);
 
 %% Optimization parameters
 myoptions   =   myoptimset;
