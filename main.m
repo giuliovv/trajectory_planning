f6f9754afd2859adc8a4575dbdb355b77b32681a
Giulio Vaccari <giulio09@marcov.it>
2021-12-04 17:03:38 +0100

    Constraints and gitignore

--- main.m
+++ main.m
@@ -7,15 +7,18 @@ w=1.992;
 e=0.40;
 
 %initialization of kr and kl, tuning of their ratio
-kl=0.5;
-kr=0.5;
+kl=100;
+kr=40;
+
+%gamma in case of constraint
+gamma = 500;
 
 %track borders
 [rightpoints, leftpoints] = read_track("tracks/IMS.csv");
 n = height(rightpoints);
 
 %% Initialization of the coefficient alpha
-alpha0 = 0.1*ones(n, 1);
+alpha0 = 0.5*ones(n, 1);
 
 %% Optimization parameters
 myoptions   =   myoptimset;
@@ -23,7 +26,7 @@ myoptions.ls_beta       = 0.3;
 myoptions.ls_c          = 0.1;
 myoptions.gradmethod    = 'UP';
 myoptions.graddx        = eps^(1/3);
-myoptions.nitermax      = 1e4;
+myoptions.nitermax      = 5e2;
 myoptions.tolfun        =	1e-12;
 myoptions.Hessmethod    = 'SD';
 %myoptions.GN_funF       = TODO
@@ -32,7 +35,7 @@ myoptions.Hessmethod    = 'SD';
 %different coordinates to pass through
 %cost function
 
-[Ustar,fxstar,k,exitflag,xsequence] = myfminunc(@(alpha)mycostfunction(n,rightpoints,leftpoints,alpha, kl, kr),alpha0,myoptions);
+[Ustar,fxstar,k,exitflag,xsequence] = myfminunc(@(alpha)mycostfunction_constrained(n,rightpoints,leftpoints,alpha, kl, kr, gamma) ,alpha0,myoptions);
 
 %% Plot
  [x,y] = mycoordinates(n,rightpoints,leftpoints,Ustar);
